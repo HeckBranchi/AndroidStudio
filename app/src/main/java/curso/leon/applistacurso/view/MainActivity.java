acc5a80f78b482e781f2eea938da1cab6b08f02f
HeckBranchi <75676176+HeckBranchi@users.noreply.github.com>
2023-04-15 15:35:30 -0300

    Adicionado Sharedpreferences

--- app/src/main/java/curso/leon/applistacurso/view/MainActivity.java
+++ app/src/main/java/curso/leon/applistacurso/view/MainActivity.java
@@ -2,12 +2,14 @@ package curso.leon.applistacurso.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.media.MediaTimestamp;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ShareActionProvider;
 import android.widget.Toast;
 
 import curso.leon.applistacurso.R;
@@ -15,6 +17,10 @@ import curso.leon.applistacurso.controller.PessoaController;
 import curso.leon.applistacurso.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences preferences;
+    SharedPreferences.Editor listavip;
+    public static final String nome_preferences = "pref listavip";
 
     PessoaController controller;
     EditText editPrimeiroNome;
@@ -33,13 +39,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Object pessoa2;
-        controller = new PessoaController(pessoa2);
+        preferences = getSharedPreferences(nome_preferences,0);
+        listavip = preferences.edit();
+        controller = new PessoaController();
         pessoa = new Pessoa();
-
-        pessoa.setNomeCurso("Programação Dispositivos Móveis");
-        pessoa.setPrimeiroNome("Leon");
-        pessoa.setSobrenomee("Heck Branchi");
-        pessoa.setTelefoneContato("51 99559955");
+        pessoa.setPrimeiroNome((preferences.getString("primeiroNome", "")));
+        pessoa.setSobrenomee((preferences.getString("sobrenome", "")));
+        pessoa.setTelefoneContato((preferences.getString("telefoneContato", "")));
+        pessoa.setNomeCurso((preferences.getString("nomeCurso", "")));
 
        Log.i("teste log","Infos: "+pessoa.toString());
 
@@ -52,6 +59,8 @@ public class MainActivity extends AppCompatActivity {
        buttonFinalizar = findViewById(R.id.buttonFinalizar);
 
        editPrimeiroNome.setText(pessoa.getPrimeiroNome());
+
+       editPrimeiroNome.setText(pessoa.getPrimeiroNome());
        editSobrenome.setText(pessoa.getSobrenomee());
        editNomeCurso.setText(pessoa.getNomeCurso());
        editTelefone.setText(pessoa.getTelefoneContato());
@@ -59,12 +68,20 @@ public class MainActivity extends AppCompatActivity {
        buttonSalvar.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
-               pessoa.setPrimeiroNome(editPrimeiroNome.getText().toString());
-               pessoa.setSobrenomee(editSobrenome.getText().toString());
-               pessoa.setNomeCurso(editNomeCurso.getText().toString());
-               pessoa.setTelefoneContato(editTelefone.getText().toString());
+              pessoa.setPrimeiroNome(editPrimeiroNome.getText().toString());
+              pessoa.setSobrenomee(editSobrenome.getText().toString());
+              pessoa.setNomeCurso(editNomeCurso.getText().toString());
+              pessoa.setTelefoneContato(editTelefone.getText().toString());
 
                Toast.makeText(MainActivity.this, "Salvo :"+pessoa.toString(), Toast.LENGTH_LONG).show();
+               listavip.putString("primeiroNome", pessoa.getPrimeiroNome());
+               listavip.putString("sobrenome", pessoa.getSobrenomee());
+               listavip.putString("telefoneContato", pessoa.getTelefoneContato());
+               listavip.putString("nomeCurso", pessoa.getNomeCurso());
+               listavip.apply();
+
+               controller.salvar(pessoa);
+
            }
        });
        buttonLimpar.setOnClickListener(new View.OnClickListener() {
@@ -74,6 +91,9 @@ public class MainActivity extends AppCompatActivity {
                editSobrenome.setText(" ");
                editNomeCurso.setText(" ");
                editTelefone.setText(" ");
+
+               listavip.clear();
+               listavip.apply();
 
            }
        });
